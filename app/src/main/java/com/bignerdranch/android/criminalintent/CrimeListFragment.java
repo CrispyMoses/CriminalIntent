a0bb3868a7ba34ffebfa9d7a934914159e208292
CrispyMoses <Gendelf4@yandex.ru>
2017-05-27 14:30:58 +0300

    commit 5

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -0,0 +1,11 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Crispy on 27.05.2017.
+ */
+
+public class CrimeListFragment extends Fragment {
+
+}
